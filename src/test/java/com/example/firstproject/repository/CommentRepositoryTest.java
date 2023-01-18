f74ce5f1fc2747d4e4161f62d8010b3f3a509bed
hosung-222 <62132755+hosung-222@users.noreply.github.com>
2023-01-18 20:09:25 +0900

    Comment CRUD function

--- src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -41,11 +41,42 @@ class CommentRepositoryTest {
         }
 
         // case 2: 1번 게시글의 모든 댓글 조회
+        {
+            // 입력 데이터 준비
+            Long articleId = 1L;
+
+            // 실제 수행
+            List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+            // 예상
+            Article article = new Article(1L,"가가가가","1111");
+            List<Comment> expected = Arrays.asList();
+            // 검증
+            assertEquals(expected.toString(), comments.toString(), "1번 글은 댓글이 없음");
+        }
 
 
     }
 
     @Test
+    @DisplayName("특정 닉네임의 모든 댓글 조회")
     void findByNickname() {
+        // case 1: "Park" 의 모든 댓글 조회
+        {
+            // 입력데이터 준비
+            String nickname = "Park";
+            // 실제 수행
+            List<Comment> comments = commentRepository.findByNickname(nickname);
+
+            // 예상하기
+            Comment a = new Comment(1L, new Article(4L,"당신의 인생 영화는?","댓글 ㄱ"), nickname, "굳 월 헌팅");
+            Comment b = new Comment(4L, new Article(5L,"당신의 소울 푸드는?","댓글 ㄱㄱ"), nickname, "치킨");
+            Comment c = new Comment(7L, new Article(6L,"당신의 취미는?","댓글 ㄱㄱㄱㄱ"), nickname, "조깅");
+            List<Comment> expected = Arrays.asList(a,b,c);
+
+            // 검증
+            assertEquals(expected.toString(), comments.toString(), "park의 모든 댓글을 출력");
+        }
+
     }
 }
